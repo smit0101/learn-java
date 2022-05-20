b803602081a467100dd56ec664a4c2853464b0f1
smit0101 <smitkalkani9099@gmail.com>
2022-05-20 13:57:32 +0530

    Project Created To Try New Lang Features

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        A a = new C();
+        int value = switch (a){
+            case null -> -1;
+            case D d -> 8;
+            case C c -> 9;
+            case B b -> 7;
+            case A obj -> 2;
+        };
+        System.out.println(value);
     }
 }
+
+record User(int a){
+    User(int a){
+        this.a = a;
+    }
+   static int aa = 10;
+}
+
+sealed class A{}
+sealed class B extends A{}
+sealed class C extends B{}
+non-sealed class D extends C{}
+
+class E extends  D{}
