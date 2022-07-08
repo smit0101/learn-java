bb11069a426c9f0f178e7de609c4c54832f18dc0
smit0101 <smitkalkani9099@gmail.com>
2022-07-08 18:31:26 +0530

    Project Created To Try New Lang Features

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         A a = new C();
@@ -9,6 +11,8 @@ public class Main {
             case A obj -> 2;
         };
         System.out.println(value++);
+
+        List.of(1,2,3,4,5,6,7,8,9).forEach(System.out::println);
     }
 }
 
